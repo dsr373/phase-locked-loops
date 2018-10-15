5be393dafa895568154e3dc428d8a9bdc24df55b
Dan Seremet <seremet.dan@gmail.com>
2018-10-15 15:37:41 +0100

    added measurement of B

--- python/measurement/test_frequency.py
+++ python/measurement/test_frequency.py
@@ -4,52 +4,66 @@ import numpy as np
 from utils.pico_utils import open_pico, configure_channel, configure_sampling, getData
 from utils.serial_utils import send_command
 from utils.analysis_utils import calc_frequency
+from utils.plotting_utils import set_as_freq, set_as_time
 
 # set constants
 frequency = float(raw_input('Enter frequency (Hz): '))
+phase_diff = int(raw_input('Enter phase difference (deg): '))
 half_p_us = 5e5/frequency  # in microseconds
 
 # send half-period to arduino
 print("Sending stuff to ARDUINO")
-send_command(half_p=half_p_us)
+send_command(half_p=half_p_us, phase_diff=phase_diff)
 
 # read signal
 print("\n\n")
 ps = open_pico()
 configure_channel(ps, 'A')
-# configure_channel(ps, 'B')
+configure_channel(ps, 'B')
 ps.setSimpleTrigger('A', 1.0, 'Falling', timeout_ms=100, enabled=True)
 
 (sampling_interval, nSamples, maxSamples) = configure_sampling(ps, 2*half_p_us/1e6)
 
 dataA = getData(ps, nSamples, channel='A')
+dataB = getData(ps, nSamples, channel='B')
 ps.stop()
 ps.close()
 
 # do the FT
 spectrum = np.fft.fft(np.array(dataA), nSamples)
+spectrumB = np.fft.fft(np.array(dataB), nSamples)
 freqs = np.fft.fftfreq(nSamples, sampling_interval)
 
-print("Frequency = %f Hz" % calc_frequency(freqs, spectrum))
+print("FrequencyA = %f Hz" % calc_frequency(freqs, spectrum))
+print("FrequencyB = %f Hz" % calc_frequency(freqs, spectrumB))
 
-# write to file
-with open('data/real2.csv', 'w') as fout:
-    fout.write('nSamples = %d\n' % nSamples)
-    fout.write('sampling_interval = %d us\n\n' % (sampling_interval*1e6))
-    for i, datum in enumerate(dataA):
-        fout.write('%d, %f\n' % (i, datum))
+# # write to file
+# with open('data/real2.csv', 'w') as fout:
+#     fout.write('nSamples = %d\n' % nSamples)
+#     fout.write('sampling_interval = %d us\n\n' % (sampling_interval*1e6))
+#     for i, datum in enumerate(dataA):
+#         fout.write('%d, %f\n' % (i, datum))
 
 
-with open('data/fourier2.csv', 'w') as fout:
-    for (f, i) in zip(freqs, abs(spectrum)):
-        fout.write('%f, %f\n' % (f, i))
+# with open('data/fourier2.csv', 'w') as fout:
+#     for (f, i) in zip(freqs, abs(spectrum)):
+#         fout.write('%f, %f\n' % (f, i))
 
 # plot the real signal
-fig1, ax1 = plt.subplots()
-ax1.plot(np.linspace(0, sampling_interval*nSamples, num=nSamples), dataA)
+t = np.linspace(0, sampling_interval*nSamples, num=nSamples)
+fig, [ax, ax1] = plt.subplots(nrows=2, ncols=1)
+
+set_as_time(ax)
+ax.plot(t, dataA, label='A')
+ax.plot(t, dataB, label='B')
+ax.legend()
 
 # plot the FT'ed signal
-fig, ax = plt.subplots()
-ax.plot(freqs, abs(spectrum))
-ax.set_xlim(left=-200, right=200)
+set_as_freq(ax1)
+ax1.plot(freqs, abs(spectrum), label='A')
+ax1.plot(freqs, abs(spectrumB), label='B')
+ax1.set_xlim(left=-4*frequency, right=4*frequency)
+ax1.legend()
+
+plt.tight_layout()
 plt.show()
