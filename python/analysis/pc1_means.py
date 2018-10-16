8e079de6153134acee21b893695f40d8647fe5f6
Dan Seremet <seremet.dan@gmail.com>
2018-10-16 21:27:42 +0100

    fixed key error

--- python/analysis/pc1_means.py
+++ python/analysis/pc1_means.py
@@ -15,11 +15,11 @@ with open('data/pc1/run1.tsv') as fin:
     for row in reader:
         # construct the measurements dict
         freq = float(row[1])
-        phase = float(row[2])
+        phase = int(row[2])
         vB = float(row[-1])
 
         if freq in measurements.keys():
-            if phase in measurements.keys():
+            if phase in measurements[freq].keys():
                 measurements[freq][phase].append(vB)
             else:
                 measurements[freq][phase] = [vB]
@@ -31,10 +31,11 @@ for freq in measurements.keys():
     for phase in measurements[freq].keys():
         results[freq][phase] = (np.mean(measurements[freq][phase]), np.std(measurements[freq][phase]))
 
+# all_freqs = sorted(list(results.keys()))
+all_freqs = [1e1, 1e2, 1e3]
+
 fig, ax = plt.subplots(figsize=(12, 8))
 matplotlib.rcParams.update({'errorbar.capsize': 5})
-
-all_freqs = sorted(list(results.keys()))
 
 for freq in all_freqs:
     xs, ys, sigmas = [], [], []
@@ -43,7 +44,7 @@ for freq in all_freqs:
         ys.append(results[freq][phase][0])
         sigmas.append(results[freq][phase][1])
 
-    ax.errorbar(xs, ys, yerr=sigmas, fmt='+', markersize=15, label='%.1e' % freq)
+    ax.errorbar(xs, ys, yerr=sigmas, fmt='+', markersize=8, label='%.1e' % freq)
 
 ax.set_title('Output of phase comparator 1', fontsize=15)
 ax.set_xlabel('Phase difference (deg)', fontsize=15)
