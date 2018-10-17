87cac697234a699f932b9f26369ef4fdc4195bbd
Dan Seremet <seremet.dan@gmail.com>
2018-10-17 15:39:27 +0100

    plot config now takes fontsize

--- python/measurement/utils/gui_utils.py
+++ python/measurement/utils/gui_utils.py
@@ -1,13 +1,15 @@
-def set_as_time(axes):
-    axes.set_title('Time Domain')
-    axes.set_xlabel('Time (s)')
-    axes.set_ylabel('Voltage (V)')
+def set_as_time(axes, fontsize=20):
+    # axes.set_title('Time Domain', fontsize=fontsize)
+    axes.set_xlabel('Time (s)', fontsize=fontsize)
+    axes.set_ylabel('Voltage (V)', fontsize=fontsize)
+    axes.tick_params(labelsize=(fontsize-2))
     axes.set_ylim(bottom=-1, top=7)
 
-def set_as_freq(axes):
-    axes.set_title('Fourier Domain')
-    axes.set_xlabel('Frequency (Hz)')
-    axes.set_ylabel('Intensity')
+def set_as_freq(axes, fontsize=20):
+    axes.set_title('Fourier Domain', fontsize=fontsize)
+    axes.set_xlabel('Frequency (Hz)', fontsize=fontsize)
+    axes.set_ylabel('Intensity', fontsize=fontsize)
+    axes.tick_params(labelsize=fontsize-2)
 
 def def_input(prompt, default='0'):
     default = str(default)
