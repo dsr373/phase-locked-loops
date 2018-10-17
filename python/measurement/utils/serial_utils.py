f32510bb656da360f59ba967bb981ff45902de09
Dan Seremet <seremet.dan@gmail.com>
2018-10-18 00:19:32 +0100

    allowed sending duty cycle

--- python/measurement/utils/serial_utils.py
+++ python/measurement/utils/serial_utils.py
@@ -8,14 +8,19 @@ time.sleep(1)
 ser.setDTR(value=0)
 time.sleep(1)
 
-def send_command(half_p=None, phase_diff=None):
+def send_command(half_p=None, phase_diff=None, duty_cycle=None):
     if half_p is not None:
-        ser.write("f" + bytes(half_p))    # test??
+        ser.write("f" + bytes(int(half_p)))
         time.sleep(1)
         print("ARDUINO: " + ser.readline().strip())
         print("ARDUINO: " + ser.readline().strip())
     if phase_diff is not None:
         ser.write("p" + bytes(phase_diff))
+        time.sleep(1)
+        print("ARDUINO: " + ser.readline().strip())
+        print("ARDUINO: " + ser.readline().strip())
+    if duty_cycle is not None:
+        ser.write("v" + bytes(duty_cycle))
         time.sleep(1)
         print("ARDUINO: " + ser.readline().strip())
         print("ARDUINO: " + ser.readline().strip())
